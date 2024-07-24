076335dea13cd8b6cb1384f96ac40c904028929b
Chuks1993 <chuksgrinage.dev@gmail.com>
2024-07-23 22:37:40 -0500

    add vercel as adaptor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,50 +1,40 @@
-import mdx from '@astrojs/mdx'
-import partytown from '@astrojs/partytown'
-import react from '@astrojs/react'
-import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
-import keystatic from '@keystatic/astro'
-import playformCompress from '@playform/compress'
-import AutoImport from 'astro-auto-import'
-import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx';
+import partytown from '@astrojs/partytown';
+import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import keystatic from '@keystatic/astro';
+import playformCompress from '@playform/compress';
+import AutoImport from 'astro-auto-import';
+import { defineConfig } from 'astro/config';
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
   site: 'https://debebsmedia.com',
   redirects: {
-    '/admin': '/keystatic',
+    '/admin': '/keystatic'
   },
   markdown: {
     shikiConfig: {
       theme: 'dracula',
-      wrap: true,
-    },
+      wrap: true
+    }
   },
   integrations: [
-    // example auto import component into blog post mdx files
-    AutoImport({
-      imports: [
-        // https://github.com/delucis/astro-auto-import
-        '@components/Admonition/Admonition.astro',
-        '@components/AffiliateLink/AffiliateLink.astro',
-        {
-          '@components/Cta/NewsletterBlogPost.astro': [
-            ['default', 'Newsletter'],
-          ],
-        },
-      ],
-    }),
-    mdx(),
-    react(),
-    keystatic(),
-    tailwind(),
-    sitemap(),
-    playformCompress(),
-    partytown({
-      config: {
-        forward: ['dataLayer.push'],
-      },
-    }),
-  ],
-})
+  // example auto import component into blog post mdx files
+  AutoImport({
+    imports: [
+    // https://github.com/delucis/astro-auto-import
+    '@components/Admonition/Admonition.astro', '@components/AffiliateLink/AffiliateLink.astro', {
+      '@components/Cta/NewsletterBlogPost.astro': [['default', 'Newsletter']]
+    }]
+  }), mdx(), react(), keystatic(), tailwind(), sitemap(), playformCompress(), partytown({
+    config: {
+      forward: ['dataLayer.push']
+    }
+  })],
+  adapter: vercel()
+});
