8444b097d3014d5af5f95b83cef5b72e5cf22d4b
Chuks1993 <chuksgrinage.dev@gmail.com>
2024-07-23 23:23:55 -0500

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import mdx from '@astrojs/mdx'
+import netlify from '@astrojs/netlify'
 import partytown from '@astrojs/partytown'
 import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
