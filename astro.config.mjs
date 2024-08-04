82ed86838c4fbb8c315ca0a5b89ad6b2d66056f4
Chuks1993 <chuksgrinage.dev@gmail.com>
2024-08-04 13:08:44 -0500

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import mdx from '@astrojs/mdx'
+import netlify from '@astrojs/netlify'
 import partytown from '@astrojs/partytown'
 import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
@@ -8,33 +9,47 @@ import playformCompress from '@playform/compress'
 import AutoImport from 'astro-auto-import'
 import { defineConfig } from 'astro/config'
 
-import vercel from "@astrojs/vercel/serverless"
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
   site: 'https://debebsmedia.com',
   redirects: {
-    '/admin': '/keystatic'
+    '/admin': '/keystatic',
   },
   markdown: {
     shikiConfig: {
       theme: 'dracula',
-      wrap: true
-    }
+      wrap: true,
+    },
   },
   integrations: [
-  // example auto import component into blog post mdx files
-  AutoImport({
-    imports: [
-    // https://github.com/delucis/astro-auto-import
-    '@components/Admonition/Admonition.astro', '@components/AffiliateLink/AffiliateLink.astro', {
-      '@components/Cta/NewsletterBlogPost.astro': [['default', 'Newsletter']]
-    }]
-  }), mdx(), react(), keystatic(), tailwind(), sitemap(), playformCompress(), partytown({
-    config: {
-      forward: ['dataLayer.push']
-    }
-  })],
-  adapter: vercel()
-});
+    // example auto import component into blog post mdx files
+    AutoImport({
+      imports: [
+        // https://github.com/delucis/astro-auto-import
+        '@components/Admonition/Admonition.astro',
+        '@components/AffiliateLink/AffiliateLink.astro',
+        {
+          '@components/Cta/NewsletterBlogPost.astro': [
+            ['default', 'Newsletter'],
+          ],
+        },
+      ],
+    }),
+    mdx(),
+    react(),
+    keystatic(),
+    tailwind(),
+    sitemap(),
+    playformCompress(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
+  adapter: netlify({
+    imageCDN: false,
+  }),
+})
