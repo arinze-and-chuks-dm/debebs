28f9fd77bcaa2768ce5c20367da3caa97fe9fc5b
Chuks1993 <chuksgrinage.dev@gmail.com>
2024-08-25 20:14:09 -0500

    switch to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import mdx from '@astrojs/mdx'
-import netlify from '@astrojs/netlify'
 import partytown from '@astrojs/partytown'
 import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
@@ -9,6 +8,7 @@ import playformCompress from '@playform/compress'
 import AutoImport from 'astro-auto-import'
 import { defineConfig } from 'astro/config'
 
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
@@ -49,7 +49,5 @@ export default defineConfig({
       },
     }),
   ],
-  adapter: netlify({
-    imageCDN: false,
-  }),
+  adapter: vercel(),
 })
